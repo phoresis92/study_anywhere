ab054890f25783d57bf97c0e166b310273f3a88d
Young <phoresis92@naver.com>
2019-02-14 00:02:30 +0900

    modify sql

--- Study_Anywhere/study_anywhere.sql
+++ Study_Anywhere/study_anywhere.sql
@@ -8,9 +8,10 @@ CREATE TABLE member(
 	member_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	member_suspended DATE, 
 	PRIMARY KEY(member_id)
-);
+) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
-insert into member(member_id, member_pw, member_email, member_checked) values('qwer', 'qwer', 'qwer@qwer.com', 1);
+insert into member(member_id, member_pw, member_email, member_checked) values('java', '1234', 'qwer@qwer.com', 1);
+insert into member(member_id, member_pw, member_email, member_checked) values('youngdong', '1234', 'qwer@qwer.com', 1);
 
 create table room(
     roomname varchar(30) not null,
@@ -18,9 +19,9 @@ create table room(
 	userid varchar(20) not null,
     indata TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     primary key(roomname)
-);
+) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
-insert into room values('lobby','1234','lobby',now());
+insert into room values('스프링 공부해요','1234','java',now());
 
 
 create table calendar(
@@ -36,7 +37,7 @@ constraint calendar_roomname_fk foreign key (roomname)
 references room(roomname) on delete cascade,
 constraint calendar_member_id_fk foreign key(member_id)
 references member(member_id) on delete cascade    
-);
+) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 	
 	
 	create table chatlist(
@@ -46,7 +47,7 @@ references member(member_id) on delete cascade
     primary key (member_id),
     constraint chatlist_member_id_fk foreign key(member_id)
     references member(member_id) on delete cascade    
-    );
+    ) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 	
 	
 	CREATE TABLE board(
@@ -62,7 +63,7 @@ references member(member_id) on delete cascade
 	REFERENCES member(member_id) ON DELETE CASCADE,
 	constraint board_roomname_fk foreign key(roomname)
 	references room(roomname) on delete cascade
-	);
+	) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 	
 	insert into board(member_id, board_subject, board_content, roomname ) values('aa','title','content','1234');
 	
@@ -77,7 +78,7 @@ references member(member_id) on delete cascade
 	REFERENCES member(member_id) ON DELETE CASCADE,
 	CONSTRAINT FK_COMMENT_ARTICLE FOREIGN KEY(board_num)
 	REFERENCES board(board_num) ON DELETE CASCADE
-);
+	) DEFAULT CHARSET=utf8 COLLATE utf8_general_ci;
 
 insert into board_comment(board_num, member_id, comment_con) values('70','aa','asdf');
 select max(comment_num) from board_comment;
